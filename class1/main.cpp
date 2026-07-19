860aa2792f9a9fcc9b94c8afca50f852b4236ad2
aurora <aurora_sy0220@163.com>
2026-07-19 21:47:27 +0800

    add class1

---
+++ class1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main()
+{
+    string _name = "li ming";
+    int _age = 18;
+    double _weight = 65.5;
+
+    cout << "name: " << _name << " age: " << _age << " weight: " << _weight;
+
+    return 0;
+}
