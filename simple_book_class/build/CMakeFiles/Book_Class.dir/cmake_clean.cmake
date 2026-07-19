07d187bb7bc4a55a2851e56ae5270a93ed61eefd
aurora <aurora_sy0220@163.com>
2026-07-19 21:35:05 +0800

    add build

---
+++ simple_book_class/build/CMakeFiles/Book_Class.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "Book_Class"
+  "Book_Class.pdb"
+  "CMakeFiles/Book_Class.dir/main.cpp.o"
+  "CMakeFiles/Book_Class.dir/main.cpp.o.d"
+  "CMakeFiles/Book_Class.dir/src/book.cpp.o"
+  "CMakeFiles/Book_Class.dir/src/book.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Book_Class.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ simple_book_class/build/CMakeFiles/Book_Class.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Book_Class.
+# This may be replaced when dependencies are built.
